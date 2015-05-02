72565ba93db3281361a03d16b124ff0b1ee2e9fc
Chris Nevett <nevett@gmail.com>
2015-05-02 16:17:46 +0100

    Useful homepage stuff

---
+++ Hack24/Hack24/Infrastructure/RazorViewExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Hack24.Core.Entities;
+using NanoIoC;
+
+namespace Hack24.Infrastructure
+{
+	public static class RazorViewExtensions
+	{
+		public static User User(this WebViewPage view)
+		{
+			return Container.Global.Resolve<Hack24.Core.Entities.User>();
+		}
+	}
+}
