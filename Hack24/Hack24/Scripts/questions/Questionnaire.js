cd0ed559457ae048df6a3f997fbc6854d0361756
Harry Boyes <harry.boyes@myunidays.com>
2015-05-02 16:41:38 +0100

    Structure in place to return answers

--- Hack24/Hack24/Scripts/questions/Questionnaire.js
+++ Hack24/Hack24/Scripts/questions/Questionnaire.js
@@ -1,5 +1,10 @@
 $(function () {
+	var $questions = $("ul");
+	var questions = [];
 
+	for (var i = 0; i < $questions.length; i++) {
+		questions.push({ id: $($questions[i]).data("questionid"), answerId: null });
+	}
 	$(".answer").click(function () {
 		$(".answer").removeClass("selected not-selected");
 
@@ -9,7 +14,27 @@ $(function () {
 		window.bus.pub("question answered", $(this).closest("ul").data("questionid"), $(this).data("answerid"));
 	});
 
-	window.bus.sub("question answered", function(questionId, answerId) {
-		console.log("Answered", questionId, answerId);
+	window.bus.sub("question answered", function (questionId, answerId) {
+		var nextQuestion;
+
+		for (var i = 0; i < questions.length; i++) {
+			if (questions[i].id == questionId)
+				questions[i].answerId = answerId;
+			else if (questions[i].answerId == null && nextQuestion == null)
+				nextQuestion = questions[i].id;
+		}
+
+		if (nextQuestion == null)
+			window.bus.pub("questionnaire finish", questions);
+		else
+			window.bus.pub("question goto", nextQuestion);
+	});
+
+	window.bus.sub("question goto", function(questionId) {
+		console.log("Go to question", questionId);
+	});
+
+	window.bus.sub("questionnaire finish", function(answers) {
+		console.log("Questionnaire finish", answers);
 	});
 });
