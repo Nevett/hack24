39e39f4e5b81e6f38f74244d7a0854bfeabd7e28
Chris Nevett <nevett@gmail.com>
2015-05-02 13:03:08 +0100

    dat ioc

---
+++ Hack24/Hack24/Infrastructure/IoCControllerFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+using NanoIoC;
+
+namespace Hack24.Infrastructure
+{
+	public class IoCControllerFactory : DefaultControllerFactory
+	{
+		private readonly IContainer container;
+
+		public IoCControllerFactory(IContainer container)
+		{
+			this.container = container;
+		}
+
+		protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
+		{
+			return container.Resolve(controllerType) as Controller;
+		}
+	}
+}
